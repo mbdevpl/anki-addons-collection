ea757a9e7cf234331122bf8f5d0cf6b7151a975b
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-01-06 15:39:22 +0900

    improved loading of "Button Colours (Good, Again)" addon

--- Button Colours (Good, Again).py
+++ Button Colours (Good, Again).py
@@ -5,4 +5,4 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), 'button_colours'))
 # sys.path.append(pathlib.Path(__file__).parent.joinpath('button_colours'))
 
-from button_colours import *
+import button_colours
