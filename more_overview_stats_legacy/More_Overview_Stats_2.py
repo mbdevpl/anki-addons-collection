ae05b661750e1fc946cc431f5da0698debdb76e1
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-01-08 15:23:03 +0900

    adapted legacy "More Overview Stats 2" addon to Anki 2.1

--- more_overview_stats_legacy/More_Overview_Stats_2.py
+++ more_overview_stats_legacy/More_Overview_Stats_2.py
@@ -182,12 +182,12 @@ def overview_table(self):
 
     cards = {}
 
-    cards['mature'] = mature / int(correction_for_notes)
-    cards['young'] = young / int(correction_for_notes)
-    cards['unseen'] = unseen / int(correction_for_notes)
-    cards['suspended'] = suspended / int(correction_for_notes)
+    cards['mature'] = round(mature / int(correction_for_notes))
+    cards['young'] = round(young / int(correction_for_notes))
+    cards['unseen'] = round(unseen / int(correction_for_notes))
+    cards['suspended'] = round(suspended / int(correction_for_notes))
 
-    cards['total'] = total / int(correction_for_notes)
+    cards['total'] = round(total / int(correction_for_notes))
     cards['learned'] = cards['mature'] + cards['young']
     cards['unlearned'] = cards['total'] - cards['learned']
 
